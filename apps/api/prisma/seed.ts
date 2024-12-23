2a8912ee1eb861304e6611d92b688551d79780c7
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-23 12:42:16 -0500

    feat(persistence): add seeders product

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,21 @@
+import { products } from './product';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const product of products) {
+    await prisma.product.create({
+      data: product,
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
