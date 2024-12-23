92ea526da37365f1e36ee20c92c5e3a1b23391fb
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-23 16:21:30 -0500

    feat(infrastructure): add abstract repositories

---
+++ apps/api/src/payments/infrastructure/adapters/repositories/transaction.repository.impl.ts
@@ -0,0 +1,20 @@
+// import { TransactionInterface } from 'src/interfaces';
+// import { Order } from 'src/payments/domain/entities/order.entity';
+// import {
+//   Transaction,
+//   TransactionStatus,
+// } from 'src/payments/domain/entities/transaction.entity';
+// import { TransactionRepository } from 'src/payments/domain/repositories/transaction.repository';
+
+// export class InMemoryTransactionRepository implements TransactionRepository {
+//   create(
+//     transaction: TransactionInterface,
+//     order: Order,
+//   ): Promise<Transaction> {}
+//   findById(id: string): Promise<Transaction | null> {}
+//   findByOrderId(orderId: string): Promise<Transaction | null> {}
+//   updateStatus(
+//     id: string,
+//     status: TransactionStatus,
+//   ): Promise<Transaction | null> {}
+// }
