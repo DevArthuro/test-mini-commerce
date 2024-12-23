7353a6985b5b13d2de6cd34d059a7c3188b6c21a
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-22 21:44:51 -0500

    fix: add card info and tokenized flow

--- apps/api/src/domain/entities/customer.entity.ts
+++ apps/api/src/domain/entities/customer.entity.ts
@@ -1,6 +1,11 @@
+import { Card } from './card.entity';
+import { Delivery } from './delivery.entity';
+
 export class Customer {
   constructor(
     public readonly id: string,
+    public readonly delivery: Delivery,
+    public readonly card: Card,
     public readonly name: string,
     public readonly lastname: string,
     public readonly email: Date,
