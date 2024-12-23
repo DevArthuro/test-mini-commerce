20e8bb2e227ee11df26e786b9b953ac680982434
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-23 06:28:21 -0500

    fix(domain): add changes into repository

--- apps/api/src/domain/repositories/customer.repository.ts
+++ apps/api/src/domain/repositories/customer.repository.ts
@@ -1,7 +1,13 @@
 import { CustomerInterface } from 'src/interfaces';
 import { Customer } from '../entities/customer.entity';
+import { Delivery } from '../entities/delivery.entity';
+import { Card } from '../entities/card.entity';
 
 export interface CustomerRepository {
-  createCustomer(customer: CustomerInterface): Promise<Customer>;
+  createCustomer(
+    customer: CustomerInterface,
+    delivery: Delivery,
+    card: Card,
+  ): Promise<Customer>;
   findById(id: string): Promise<Customer | null>;
 }
