b4b027544857cd9dc13cdbd98dd779215b65458e
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-23 15:48:09 -0500

    fix: remove unknown methods

--- apps/api/src/payments/domain/repositories/customer.repository.ts
+++ apps/api/src/payments/domain/repositories/customer.repository.ts
@@ -2,12 +2,13 @@ import { CustomerInterface } from 'src/interfaces';
 import { Customer } from '../entities/customer.entity';
 import { Delivery } from '../entities/delivery.entity';
 import { Card } from '../entities/card.entity';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export abstract class CustomerRepository {
   abstract createCustomer(
     customer: CustomerInterface,
     delivery: Delivery,
     card: Card,
   ): Promise<Customer>;
-  abstract findById(id: string): Promise<Customer | null>;
 }
--- apps/api/src/payments/domain/repositories/delivery.repository.ts
+++ apps/api/src/payments/domain/repositories/delivery.repository.ts
@@ -2,6 +2,5 @@ import { DeliveryInterface } from 'src/interfaces';
 import { Delivery } from '../entities/delivery.entity';
 
 export abstract class DeliveryRepository {
-  abstract createDelivery(customer: DeliveryInterface): Promise<Delivery>;
-  abstract findById(id: string): Promise<Delivery | null>;
+  abstract createDelivery(delivery: DeliveryInterface): Promise<Delivery>;
 }
