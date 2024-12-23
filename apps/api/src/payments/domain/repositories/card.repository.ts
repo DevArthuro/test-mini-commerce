117a9e2b012a247470340a818f0ebc116a394374
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-23 15:48:09 -0500

    fix: remove unknown methods

--- apps/api/src/payments/domain/repositories/card.repository.ts
+++ apps/api/src/payments/domain/repositories/card.repository.ts
@@ -3,5 +3,4 @@ import { Card } from '../entities/card.entity';
 
 export abstract class CardRepository {
   abstract createCard(card: CardInterface): Promise<Card>;
-  abstract findById(id: string): Promise<Card | null>;
 }
