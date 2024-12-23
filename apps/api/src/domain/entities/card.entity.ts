d69d0ac06a60b42053cd2da903a84017b8f0d0d9
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-22 22:41:43 -0500

    test: add test card entity

--- apps/api/src/domain/entities/card.entity.ts
+++ apps/api/src/domain/entities/card.entity.ts
@@ -7,4 +7,26 @@ export class Card {
     public readonly expYear: string,
     public readonly cardName: string,
   ) {}
+
+  public getLatestDigitsCard() {
+    return this.number.slice(-4);
+  }
+
+  public getConcatDates() {
+    return `${this.expMonth}/${this.expYear}`;
+  }
+
+  public toValue(): VISIBILITY_CARD_INFO {
+    return {
+      number: this.getLatestDigitsCard(),
+      exp_date: this.getConcatDates(),
+      cardName: this.cardName,
+    };
+  }
+}
+
+export interface VISIBILITY_CARD_INFO {
+  number: string;
+  exp_date: string;
+  cardName: string;
 }
