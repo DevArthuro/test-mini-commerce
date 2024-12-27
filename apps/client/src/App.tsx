c1d5717c649fb4ee233198579d6340153f8f8778
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-27 14:23:35 -0500

    refactor: change client by template react jest

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-
 function App() {
-  return (
-    <>
-      <p>Hola mundo</p>
-    </>
-  );
+	return (
+		<div className='App'>
+			<div className='blue-color'>
+				hello
+				<div className='green-color'>world</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
-import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-      },
-    },
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
-});
-
-
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3500',
+			},
+		},
+	},
+})
