3c1ddd53ff66f7c31f9230271302acc7adacbaa0
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-27 01:50:57 -0500

    fix: uninstall jest is not working

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,17 +2,9 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <button
-        onClick={async () => {
-          const response = await fetch("/api/v1");
-          const data = await response.text();
-          console.log(data);
-        }}
-      >
-        On click
-      </button>
-    </div>
+    <>
+      <p>Hola mundo</p>
+    </>
   );
 }
 
--- apps/client/src/__tests__/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import App from "../App";
-import { render } from "@testing-library/react";
-
-describe("App test", () => {
-  it("should render App component", () => {
-    render(<App />);
-  });
-});
