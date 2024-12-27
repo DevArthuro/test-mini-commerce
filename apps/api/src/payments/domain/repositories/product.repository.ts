a02b1456ffce5210f2eadd27cbeb1f35a9737897
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-26 20:33:08 -0500

    fix: improve the manage stock

--- apps/api/src/payments/domain/repositories/product.repository.ts
+++ apps/api/src/payments/domain/repositories/product.repository.ts
@@ -3,5 +3,12 @@ import { Product } from '../entities/product.entity';
 export abstract class ProductRepository {
   abstract findAll(): Promise<Product[]>;
   abstract findById(id: string): Promise<Product | null>;
-  abstract updateStock(id: string, quantity: number): Promise<Product | null>;
+  abstract updateStockDecrease(
+    id: string,
+    quantity: number,
+  ): Promise<Product | null>;
+  abstract updateStockIncrease(
+    id: string,
+    quantity: number,
+  ): Promise<Product | null>;
 }
