b3600dbf30b2f65c0a40fff05f34a297b23a2632
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-27 01:50:57 -0500

    fix: uninstall jest is not working

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
         target: "http://localhost:3000",
       },
     },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
   },
 });
 
