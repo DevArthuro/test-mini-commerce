8a26c3a42bf1e2e56f124ed443291acbbe2404f5
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-29 06:54:20 -0500

    fix: add fixes products screen

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import { ROUTES } from "./utils/route"
+import "./globals.scss"
 
 function App() {
 	return (
--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -1,15 +1,26 @@
-import { Suspense } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import "./navbar.scss";
 
 const Navbar = () => {
-	return (
-		<div>
-			Navbar
-			<Suspense fallback={<h1>Loading...</h1>}>
-				<Outlet />
-			</Suspense>
-		</div>
-	)
-}
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar__container">
+				  <div className="navbar__brand">
+					  <img src="" alt="" />
+            <h1>MiniCommerce.com</h1>
+          </div>
+          <div className="navbar__right">
+            <button className="navbar__button">Ver pedido</button>
+          </div>
+        </div>
+      </div>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
