3308da1973649e8c64d2f6193563cc2afd71a4f7
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-28 22:29:40 -0500

    feat: add form payments

---
+++ apps/client/src/pages/Home/components/modalBuyProduct/formInputs/customPhoneInput.tsx
@@ -0,0 +1,34 @@
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+
+interface CustomPhoneInputProps {
+  value: string;
+  onChange: (value: any) => void;
+  error?: string;
+  label?: string;
+  className: string
+}
+
+const CustomPhoneInput: React.FC<CustomPhoneInputProps> = ({
+  value,
+  onChange,
+  error,
+  label,
+  className
+}) => {
+  return (
+    <div className={className}>
+      {label && <label>{label}</label>}
+      <PhoneInput
+        international
+        countryCallingCodeEditable={false}
+        defaultCountry="CO"
+        value={value}
+        onChange={onChange}
+      />
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
+
+export default CustomPhoneInput;
