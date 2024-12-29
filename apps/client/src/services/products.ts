ebdb70430c94b05429c5c0f5b9efdcb01f48ae11
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-29 07:32:56 -0500

    feat: create types and services to order

--- apps/client/src/services/products.ts
+++ apps/client/src/services/products.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 import { ResponseProduct } from "../types/products";
 
-
 export class Products {
-  constructor() {}
+  private fetchInstance;
+  constructor() {
+    this.fetchInstance = axios.create({
+      baseURL: import.meta.env.VITE_BASE_API_URL,
+    });
+  }
   public async getProducts(): Promise<ResponseProduct | string> {
     try {
-      const response = await axios.get<ResponseProduct>(
-        `${import.meta.env.VITE_BASE_API_URL}/products`
-      );
+      const response =
+        await this.fetchInstance.get<ResponseProduct>(`/products`);
       console.log(response.data);
       return response.data;
     } catch (error) {
