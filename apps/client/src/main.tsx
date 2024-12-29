af0a77d1fd1ba02e620210407a35d363839494d5
Carlos Orrego <carlosorrego5151@gmail.com>
2024-12-29 11:55:24 -0500

    feat: add redux persist

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,19 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import store from './store/store';
+import store, { persistor } from './store/store';
 import { fetchPoducts } from './store/slice/products.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 store.dispatch(fetchPoducts())
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- apps/client/src/store/store.ts
+++ apps/client/src/store/store.ts
@@ -1,17 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./slice/products"
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import productReducer from "./slice/products";
 import orderReducer from "./slice/orders";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { persistStore } from "redux-persist";
+import { thunk } from "redux-thunk";
 
-const store = configureStore({
-  reducer: {
-    products: productReducer,
-    orders: orderReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+  congfig: {
+    whitelist: ["orders"],
   },
+};
+
+const rootReducer = combineReducers({
+  orders: orderReducer,
+  products: productReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+        ignoredPaths: ["register", "rehydrate"],
+      },
+    }).concat(thunk),
+});
 
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
