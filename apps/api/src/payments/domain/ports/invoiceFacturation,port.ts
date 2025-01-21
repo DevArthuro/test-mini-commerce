188b7a5aef3e747d13ad4337e1688ffa9362de72
carlos orrego <carlosorrego5151@gmail.com>
2025-01-20 23:24:49 -0500

    feat: add definitions and invocations

--- apps/api/src/payments/domain/ports/invoiceFacturation,port.ts
+++ apps/api/src/payments/domain/ports/invoiceFacturation,port.ts
@@ -1,1 +1,7 @@
-export abstract class InvoiceFacturation {}
+import { CREATE_INVOICE } from 'src/payments/infrastructure/adapters/services/interfaces/factus';
+import { Invoice } from '../entities/invoice.entity';
+import { Transaction } from '../entities/transaction.entity';
+
+export abstract class InvoiceFacturation {
+  abstract createInvoice(transaction: Transaction): Promise<CREATE_INVOICE>;
+}
