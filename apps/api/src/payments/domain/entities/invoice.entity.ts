c8b7769cc00a2d9ede425004bd59185525a6ef1f
carlos orrego <carlosorrego5151@gmail.com>
2025-01-26 15:45:05 -0500

    feat: implement get invoice factus

--- apps/api/src/payments/domain/entities/invoice.entity.ts
+++ apps/api/src/payments/domain/entities/invoice.entity.ts
@@ -1,17 +1,13 @@
-import { Transaction, VISIBILITY_TRANSACTION_INFO } from './transaction.entity';
-
 export class Invoice {
   constructor(
     public readonly id: string,
     public readonly linksInvoice: linksInvoice[],
-    public readonly transaction: Transaction,
   ) {}
 
   public toValue(): VISIBILITY_INVOICE_INFO {
     return {
       id: this.id,
       linksInvoice: this.linksInvoice,
-      transaction: this.transaction.toValue(),
     };
   }
 }
@@ -19,7 +15,6 @@ export class Invoice {
 export interface VISIBILITY_INVOICE_INFO {
   id: string;
   linksInvoice: linksInvoice[];
-  transaction: VISIBILITY_TRANSACTION_INFO;
 }
 
 export interface linksInvoice {
